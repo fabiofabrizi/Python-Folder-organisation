c9b9d8ed287781f995f9613bcf08fddd14a16e64
Fabio Fabrizi <fabio.fabrizi@gmail.com>
2025-11-11 15:38:40 +0000

    Created a config file to specify directory and search terms

---
+++ config.py
@@ -0,0 +1,23 @@
+"""
+
+This is the file to specify the directory to sort and the search terms
+
+"""
+
+# Specify the target directory where the script has to work
+directory = r'C:\Users\fabio\Downloads' 
+
+# 2. Dictionary of folders and matching terms
+# The key is the subfolder and the value (list) are the terms to match
+terms_dict = {
+    "Periodization": ["Triphasic", "Periodization", "Periodized"],
+    "Sprints": ["Vince", "Acceleration", "Velocity", "400m", "100m", "30"],
+    "Data_Analysis": ["report", "monthly", "summary"],
+    "Python" : ["Python", "Machine", "Algorithms", "Statistical", "Statistics"],
+    "distance" : ["Endurance", "Distance"],
+    "hurdles" : ["Hurdles", "100mH", "400mH", "60mH"],
+    "CE": ["Combined", "Decathlon", "Heptathlon", "Pentathlon"],
+    "Contrast_Training" : ["Contrast", "French"], 
+    "Munster AAI" : ["Munster", "Ireland"],
+    "WMAC" : ["WMAC", "Muskerry"]
+}
